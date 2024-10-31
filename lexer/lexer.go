3a70d32dbf8e3f8f4851d6e41072295e33d14de3
Vitalii Kohut <vkohut@intelliarts.com>
2024-10-31 13:28:35 +0200

    feat: parse identifiers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,10 @@
 package lexer
 
-import "github.com/apex-woot/monkey-v0/token"
+import (
+	"fmt"
+
+	"github.com/apex-woot/monkey-v0/token"
+)
 
 type Lexer struct {
 	input        string
@@ -47,12 +51,33 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		fmt.Printf("reading=%q position=%d readPosition=%d\n", l.ch, l.position, l.readPosition)
+		l.readChar()
+	}
+	fmt.Printf("the literal is %q, position %d readPosition %d", l.input[position:l.position], position, l.position)
+	return l.input[position:l.position]
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
