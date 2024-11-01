6becf3c821804535115abd917b8bf40917cf67b7
Vitalii Kohut <vkohut@intelliarts.com>
2024-11-01 08:58:09 +0200

    chore: enchance example

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"github.com/apex-woot/monkey-v0/lexer"
+	"github.com/apex-woot/monkey-v0/token"
 )
 
 func main() {
-	fmt.Println("It works")
+	input := `let five = 5;
+    let ten = 10;
+    let add = fn(x, y) {
+        x + y;
+    };
+    let result = add(five, ten);
+    !-/*5;
+    5 < 10 > 5;
+
+    if (5 < 10) {
+        return true;
+    } else {
+        return false;
+    };
+    10 == 10;
+    10 != 9;
+    `
+
+	l := lexer.New(input)
+	for l.NextToken().Type != token.EOF {
+		l.NextToken()
+	}
 }
